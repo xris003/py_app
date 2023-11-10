a3d5bbdbdcc861592eefa07ffe1df8f66e80bc26
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2023-11-10 21:36:03 +0100

    mediaupload

--- myblog/urls.py
+++ myblog/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path
 from django.conf.urls import include
 
@@ -24,3 +26,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(writeups_urls, namespace='writeups'))
 ] 
+
+if settings.DEBUG: 
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
