1c0500f5fef3fb72c299e1d453829ca3cef4cf79
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2023-12-26 17:15:45 +0100

    homepage

--- myblog/urls.py
+++ myblog/urls.py
@@ -18,7 +18,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path
-from django.conf.urls import include, url
+from django.conf.urls import include, urls
 
 from writeups import urls as writeups_urls 
 from feed import urls as feed_urls 
@@ -28,7 +28,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(writeups_urls, namespace='writeups')),
     path('', include(feed_urls, namespace='feed')),
-    url("", include("allauth.urls"))
+    urls("", include("allauth.urls"))
 ] 
 
 if settings.DEBUG: 
